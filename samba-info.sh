88ca4a8cd69107304d590b40ee734227f6568059
Louis van Belle <thctlo@gmail.com>
2017-12-20 16:38:34 +0100

    changed the SAMBA_DC_* detection, thanks Roy Eastwood for reporting

--- samba-info.sh
+++ samba-info.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 ##
-## Version      : 1.0.0
-## release d.d. : 21-11-2016
+## Version      : 1.1.0
+## release d.d. : 20-12-2017
 ## Author       : L. van Belle
 ## E-mail       : louis@van-belle.nl
 ## Copyright    : Free as free can be, copy it, change it if needed.
+## Sidenote     : if you change things, please inform me
 
-# This script checks you setup for the basic settings.
-#
+# This script checks you setup for the basic settings. 
+# 
 
 BLACK=$(tput setaf 0)
 RED=$(tput setaf 1)
@@ -54,7 +55,7 @@ SETFQDN=`hostname -f`
 ## the ip of the server, if you resolv.conf is correctly setup.
 SETSERVERIP1=`hostname -i`
 SETSERVERIP2=`hostname -I`
-if [ "${SETSERVERIP1}" = "${SETSERVERIP2}" ]; then
+if [ "${SETSERVERIP1}" = "${SETSERVERIP2}" ]; then 
     SETSERVERIP="${SETSERVERIP1}"
 else
     SETSERVERIP="${SETSERVERIP2}"
@@ -66,9 +67,10 @@ fi
 
 check_run_as_sudo_root
 
-SAMBA_DC_FSMO=$(samba-tool fsmo show | cut -d"," -f2 | head -n1 | cut -c4-100)
-SAMBA_DC_FSMO_SITE=$(samba-tool fsmo show | cut -d"," -f4 | head -n1 | cut -c4-100)
-SAMBA_DC_NC=$(samba-tool fsmo show | cut -d"," -f7,8,9 | head -n1)
+# Added -H now it also works for a member server. ( thanks Roy Eastwood for reporting )
+SAMBA_DC_FSMO=$(samba-tool fsmo show -H ldap://${SETDNSDOMAIN} | cut -d"," -f2 | head -n1 | cut -c4-100)
+SAMBA_DC_FSMO_SITE=$(samba-tool fsmo show -H ldap://${SETDNSDOMAIN} | cut -d"," -f4 | head -n1 | cut -c4-100)
+SAMBA_DC_NC=$(samba-tool fsmo show -H ldap://${SETDNSDOMAIN} | cut -d"," -f7,8,9| head -n1)
 
 ## get DC's
 DCS=$(host -t SRV _kerberos._udp.${SETDNSDOMAIN} | awk '{print $NF}'| sed 's/.$//')
@@ -92,16 +94,16 @@ SAMBA_NT_DOMAINNAME=$(samba-tool domain info ${SAMBA_DC1} | grep Netbios | cut -
 SAMBA_KERBEROS_NAME=$(cat /etc/krb5.conf | grep default_realm | cut -d"=" -f2 | cut -c2-100)
 
 #
-echo "This script was tested with Debian Jessie."
+echo "This script was tested with Debian Jessie and Stretch"
 echo "Server info:                    detected           (command and where to look)"
-echo "This server hostname          = ${SETHOSTNAME}    (hostname -s and /etc/hosts and DNS server)"
-echo "This server FQDN (hostname)   = ${SETFQDN}        (hostname -f and /etc/hosts and DNS server)"
-echo "This server primary dnsdomain = ${SETDNSDOMAIN}   (hostname -d and /etc/resolv.conf and DNS server)"
-echo "This server IP address(ses)   = ${SETSERVERIP}    (hostname -i (-I) and /etc/networking/interfaces and DNS server"
-echo "The DC with FSMO roles        = ${SAMBA_DC_FSMO}  (samba-tool fsmo show)"
-echo "The DC (with FSMO) Site name  = ${SAMBA_DC_FSMO_SITE}     (samba-tool fsmo show)"
-echo "The Default Naming Context    = ${SAMBA_DC_NC}    (samba-tool fsmo show)"
-echo "The Kerberos REALM name used  = ${SAMBA_KERBEROS_NAME}    (kinit and /etc/krb5.conf and resolving)"
+echo "This server hostname          = ${SETHOSTNAME}	(hostname -s and /etc/hosts and DNS server)"
+echo "This server FQDN (hostname)   = ${SETFQDN}	(hostname -f and /etc/hosts and DNS server)"
+echo "This server primary dnsdomain = ${SETDNSDOMAIN}	(hostname -d and /etc/resolv.conf and DNS server)"
+echo "This server IP address(ses)   = ${SETSERVERIP}	(hostname -i (-I) and /etc/networking/interfaces and DNS server"
+echo "The DC with FSMO roles        = ${SAMBA_DC_FSMO}	(samba-tool fsmo show)"
+echo "The DC (with FSMO) Site name  = ${SAMBA_DC_FSMO_SITE}	(samba-tool fsmo show)"
+echo "The Default Naming Context    = ${SAMBA_DC_NC}	(samba-tool fsmo show)"
+echo "The Kerberos REALM name used  = ${SAMBA_KERBEROS_NAME}	(kinit and /etc/krb5.conf and resolving)"
 
 if [ -z "${SAMBA_DC2}" ]; then
     SAMBA_DC1_IP=$(host -t A ${SAMBA_DC1} | awk '{print $NF}')
