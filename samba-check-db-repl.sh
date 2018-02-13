41a6b839d831b4a1ddd082c1f2f8cd1d14314dcb
Louis van Belle <louis@van-belle.nl>
2018-02-13 10:05:12 +0100

    fixed not used SAMBA_LDAPCMD_FILTER, serveral other fixes and code optimization.

--- samba-check-db-repl.sh
+++ samba-check-db-repl.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -v
+
 ##
-## Version      : 1.0.6
+## Version      : 1.0.7
 ## release d.d. : 24-03-2015
 ## Author       : L. van Belle
 ## E-mail       : louis@van-belle.nl
@@ -11,8 +12,9 @@
 ## 22-04-2015   : 1.0.3 moved mailx part within check if an e-mail adres is used.
 ## 24-04-2015   : 1.0.4 added extra check, so if no is if found, you get an error message and not a python error.
 ## 21-11-2016   : 1.0.5 extra filter options. ( samba 4.5.x needs adjusting )
-# ( removed the . in the hostname resolving for the DCS, this was no error, but its more clear what people want to see )
-## 12-02-2018   : 1.0.6 fix  the test for presence of "FAILURE" will be true even if the actual result is "successful".
+## ( removed the . in the hostname resolving for the DCS, this was no error, but its more clear what people want to see )
+## 12-02-2018   : 1.0.6 fix the test for presence of "FAILURE" will be true even if the actual result is "successful".
+## 13-02-2018   : 1.0.7 fix filter, variable was not used. optimized code, remove ^M.
 
 ## Samba database checker. ( ! TESTED ON DEBIAN JESSIE samba 4.1.x-4.5.x )
 ## This script wil check for error in the samba databases with samba-tool
@@ -46,10 +48,19 @@ SAMBA_NT_ADMIN_PASS=""
 ## keep both set to yes, thats the best.
 SAMBA_CHECKDB_WITH_DRS="yes"
 SAMBA_CHECKDB_WITH_LDAPCMD="yes"
+## Filter non-synced attributes
 ## Change the filter to avoid mismatching, some items can be ignored.
 ## Some examples. : whenChanged,usnChanged,usnCreated,msDS-NcType,serverState
 ## add them with "," seperated.
-SAMBA_LDAPCMD_FILTER="whenChanged"
+SAMBA_LDAPCMD_FILTER="whenChanged,dc,DC"
+
+# TODO, this one is not integrated yet! 
+## Compare single AD partitions on Domain Controller DC1 and DC2:
+## You can compair also only one for more partitions in stead of the full DB.
+## The options are : domain configuration schema dnsdomain dnsforest
+## Keep empty for full DB compair, or space separated partition options.
+#SAMBA_LDAPCMD_PARTITIONS=""
+
 
 ## The email adress to report to.
 ## If you put an e-mail adres here the script wil also check for mail tools.. etc
@@ -72,11 +83,11 @@ POSTFIX_RELAY_HOST=""
 
 ## cleanup the log in /tmp
 ## can be handy if you want to review manualy.
-SETREMOVELOG="yes"
+SETREMOVELOG="no"
 
 ## So you reached the end for the configure..
 ## Set this one to yes.. and your good to go.
-CONFIGURED="no"
+CONFIGURED="yes"
 
 
 #######################################################################
@@ -84,42 +95,42 @@ CONFIGURED="no"
 #######################################################################
 
 ## hostname in single word, but you dont need to change this
-SETHOSTNAME=`hostname -s`
+SETHOSTNAME="$(hostname -s)"
 ## domainname.tld, but if you installed correct, you dont need to change this
-SETDNSDOMAIN=`hostname -d`
+SETDNSDOMAIN="$(hostname -d)"
 ## hostname.domainname.tld, but if you installed correct, you dont need to change this
-SETFQDN=`hostname -f`
+SETFQDN="$(hostname -f)"
 
 
-SETTPUT=`which tput`
-if [ -z ${SETTPUT} ]; then
+SETTPUT="$(which tput)"
+if [ -z "${SETTPUT}" ]; then
     echo "program tput not found, installing it now.. please wait"
     apt-get update > /dev/null
     apt-get install -y --no-install-recommends ncurses-bin > /dev/null
 fi
 
-RED=$(${SETTPUT} setaf 1)
-NORMAL=$(${SETTPUT} sgr0)
-GREEN=$(${SETTPUT} setaf 2)
-YELLOW=$(${SETTPUT} setaf 3)
-UNDERLINE=$(${SETTPUT} smul)
-WHITE=$(${SETTPUT} setaf 7)
-BOLD=$(${SETTPUT} bold)
+RED="$(${SETTPUT} setaf 1)"
+NORMAL="$(${SETTPUT} sgr0)"
+GREEN="$(${SETTPUT} setaf 2)"
+YELLOW="$(${SETTPUT} setaf 3)"
+UNDERLINE="$(${SETTPUT} smul)"
+WHITE="$(${SETTPUT} setaf 7)"
+BOLD="$(${SETTPUT} bold)"
 
 message() {
-  printf "%40s\n" "${WHITE}${BOLD}$@${NORMAL}"
+  printf "%40s\n" "${WHITE}${BOLD}$*${NORMAL}"
 }
 good() {
-  printf "%40s\n" "${GREEN}$@${NORMAL}"
+  printf "%40s\n" "${GREEN}$*${NORMAL}"
 }
 error() {
-  printf "%40s\n" "${RED}$@${NORMAL}"
+  printf "%40s\n" "${RED}$*${NORMAL}"
 }
 warning() {
-  printf "%40s\n" "${YELLOW}$@${NORMAL}"
+  printf "%40s\n" "${YELLOW}$*${NORMAL}"
 }
 warning_underline() {
-  printf "%40s\n" "${YELLOW}${UNDERLINE}$@${NORMAL}"
+  printf "%40s\n" "${YELLOW}${UNDERLINE}$*${NORMAL}"
 }
 
 check_run_as_sudo_root() {
@@ -148,45 +159,48 @@ if [ $SAMBA_CHECKDB_WITH_DRS = "no" ] && [ ${SAMBA_CHECKDB_WITH_LDAPCMD} = "no" 
     exit 0
 fi
 
-SET_SAMBATOOL=`which samba-tool`
-if [ -z $SET_SAMBATOOL ]; then
+SET_SAMBATOOL="$(which samba-tool)"
+if [ -z "$SET_SAMBATOOL" ]; then
     error "No samba-tool tool found, this script wil exit now.. this.. I cant fix."
     exit 0
 fi
 
-SET_TR=`which tr`
-if [ -z $SET_TR ]; then
+SET_TR="$(which tr)"
+if [ -z "$SET_TR" ]; then
     warning "No tr tool found, running apt-get update and install coreutils, please wait.."
     apt-get update > /dev/null
     apt-get install -y --no-install-recommends coreutils > /dev/null
     sleep 0.5
-    SET_TR=`which tr`
+    SET_TR="$(which tr)"
 fi
 
 ## get DC info
-DCS=$(host -t SRV _kerberos._udp.${SETDNSDOMAIN} | awk '{print $NF}'| sed 's/.$//')
+DCS="$(host -t SRV _kerberos._udp."${SETDNSDOMAIN}" | awk '{print $NF}'| sed 's/.$//')"
 if [ -z "${DCS}" ]; then
     error "No Samba DCS found, host -t SRV _kerberos.udp.${SETDNSDOMAIN} returned nothing"
     error "exitting now..."
     exit 0
 fi
 
-SAMBA_DC_FSMO=`echo $(${SET_SAMBATOOL} fsmo show | cut -d"," -f2 | head -n1 | cut -c4-100) | ${SET_TR} '[:upper:]' '[:lower:]'`
-SAMBA_DC1=${SAMBA_DC_FSMO}.${SETDNSDOMAIN}
+#SAMBA_DC_FSMO=(${SET_SAMBATOOL} fsmo show | cut -d',' -f2 | head -n1 | cut -c4-100 | ${SET_TR} '[:upper:]' '[:lower:]')
+SAMBA_DC_FSMO=$(echo $(${SET_SAMBATOOL} fsmo show | cut -d"," -f2 | head -n1 | cut -c4-100) | ${SET_TR} '[:upper:]' '[:lower:]')
+SAMBA_DC1="${SAMBA_DC_FSMO}.${SETDNSDOMAIN}"
 if [ -z "${SAMBA_DC1}" ]; then
     error "No Samba DC Found with FSMO Roles, you might have dns problems"
     error "exitting now..."
     exit 0
 fi
 
-SAMBA_DCS=$(echo "$DCS" | grep -v ${SAMBA_DC_FSMO})
+#SAMBA_DCS="$(echo ${DCS} | grep -v ${SAMBA_DC_FSMO})"
+SAMBA_DCS=$(echo "$DCS" | grep -v "${SAMBA_DC_FSMO}")
 if [ -z "${SAMBA_DCS}" ]; then
     error "No Samba DC's Found with, you might have dns problems"
     error "exitting now..."
+    echo $SAMBA_DCS
     exit 0
 fi
 
-SAMBA_NT_DOMAINNAME=$($SET_SAMBATOOL domain info ${SAMBA_DC1} | grep Netbios | cut -d":" -f2 | cut -c2-100)
+SAMBA_NT_DOMAINNAME="$($SET_SAMBATOOL domain info "${SAMBA_DC1}" | grep Netbios | cut -d":" -f2 | cut -c2-100)"
 if [ -z "${SAMBA_NT_DOMAINNAME}" ]; then
     error "No Samba NT DOMAIN Name found"
     error "exitting now..."
@@ -197,25 +211,25 @@ if [ -z "${SAMBA_NT_ADMIN_PASS}" ]; then
     while [ "${SAMBA_NT_ADMIN_PASS}" = "" ]; do
         message "No password for user ${SAMBA_NT_DOMAINNAME}\\${SAMBA_NT_ADMIN_USER} was set in this script!"
         warning_underline "Please enter the password for ${SAMBA_NT_DOMAINNAME}\\${SAMBA_NT_ADMIN_USER} : "
-        read -s -e SAMBA_NT_ADMIN_PASS
+        read -r -s -e "SAMBA_NT_ADMIN_PASS"
     done
 fi
 
-echo ${SAMBA_NT_ADMIN_PASS} | kinit ${SAMBA_NT_ADMIN_USER} 2&> /dev/null
+echo "${SAMBA_NT_ADMIN_PASS}" | kinit "${SAMBA_NT_ADMIN_USER}" > /dev/null
 
 
-SET_DEBCONF_SETSELECT=`which debconf-set-selections`
-if [ -z ${SET_DEBCONF_SETSELECT} ]; then
+SET_DEBCONF_SETSELECT="$(which debconf-set-selections)"
+if [ -z "${SET_DEBCONF_SETSELECT}" ]; then
     warning "No debconf-set-selections tool found, running apt-get update and install debconf , please wait.."
     apt-get update > /dev/null
     apt-get install -y --no-install-recommends debconf  > /dev/null
     sleep 0.5
-    SET_DEBCONF_SETSELECT=`which debconf-set-selections`
+    SET_DEBCONF_SETSELECT="$(which debconf-set-selections)"
 fi
 
-if [ ! -z ${EMAIL_REPORT_ADDRESS} ]; then
-    SET_SENDMAIL=`which sendmail`
-    if [ -z ${SET_SENDMAIL} ]; then
+if [ ! -z "${EMAIL_REPORT_ADDRESS}" ]; then
+    SET_SENDMAIL="$(which sendmail)"
+    if [ -z "${SET_SENDMAIL}" ]; then
         warning "No mailserver found, running apt-get update and installing postfix as smarthost, please wait.."
         ## these are the debian defaults for a "smarthost setup"
         echo "postfix postfix/main_mailer_type        select  Satellite system" | ${SET_DEBCONF_SETSELECT}
@@ -224,21 +238,21 @@ if [ ! -z ${EMAIL_REPORT_ADDRESS} ]; then
         apt-get update > /dev/null
         apt-get install -y --no-install-recommends postfix  > /dev/null
         sleep 0.5
-        SET_SENDMAIL=`which sendmail`
+        SET_SENDMAIL="$(which sendmail)"
         postconf -e "mydestination = ${SETFQDN}, localhost, localhost.localdomain"
         postconf -e "inet_interfaces = 127.0.0.1"
         postconf -e "inet_protocols = ipv4"
         sleep 0.2
         service postfix restart
     fi
-    SET_MAILTOOL=`which mail`
-    if [ -z $SET_MAILTOOL ]; then
+    SET_MAILTOOL="$(which mail)"
+    if [ -z "$SET_MAILTOOL" ]; then
         warning "No mail tool found, running apt-get update and install heirloom-mailx, please wait.."
         apt-get update > /dev/null
         apt-get install -y --no-install-recommends heirloom-mailx > /dev/null
         sleep 0.5
-        SET_MAILTOOL=`which mail`
-        cat /etc/hosts | ${SET_MAILTOOL} -s "Test mail from script : check db"  $EMAIL_REPORT_ADDRESS
+        SET_MAILTOOL="$(which mail)"
+        ${SET_MAILTOOL} -s "Test mail from script : check db"  "${EMAIL_REPORT_ADDRESS}" < /etc/hosts
     fi
 fi
 
@@ -259,28 +273,28 @@ expected_success=0
 ## expected failure is always 0
 expected_failure=0
 for x in ${SAMBA_DCS}; do
-    expected_success=$((expected_success +=10 ))
+     expected_success=$(( expected_success +=10 ))
 done
 
-if [ ! -z ${EMAIL_REPORT_ADDRESS} ]; then
+if [ ! -z "${EMAIL_REPORT_ADDRESS}" ]; then
     if [ "${SAMBA_CHECKDB_WITH_LDAPCMD}" = "yes" ]; then
         for x in ${SAMBA_DCS}; do
-            $SET_SAMBATOOL ldapcmp --filter="$(LDAPCMD_FILTER)" ldap://$SAMBA_DC1 ldap://$x  -d0 2>&1 > /tmp/samba_ldapcmp_checkdb
+            $SET_SAMBATOOL ldapcmp --filter="$(LDAPCMD_FILTER)" ldap://"${SAMBA_DC1}" ldap://"${x}" -d0  > /tmp/samba_ldapcmp_checkdb 2>&1
             if grep -q FAILURE /tmp/samba_ldapcmp_checkdb; then
-                cat /tmp/samba_ldapcmp_checkdb | ${SET_MAILTOOL} -s "FAILURE ldapcmp between $SETDCFSMO and $x" $EMAIL_REPORT_ADDRESS
+                ${SET_MAILTOOL} -s "FAILURE ldapcmp between $SETDCFSMO and $x" "${EMAIL_REPORT_ADDRESS}" < /tmp/samba_ldapcmp_checkdb
             fi
         done
     fi
     if [ "${SAMBA_CHECKDB_WITH_DRS}" = "yes" ]; then
-        ${SET_SAMBATOOL} drs showrepl -d0 2>&1 > /tmp/samba_drs_showrepl
+        ${SET_SAMBATOOL} drs showrepl -d0  > /tmp/samba_drs_showrepl 2>&1
         failure=$(grep -c "failed" /tmp/samba_drs_showrepl)
         success=$(grep -c "successful" /tmp/samba_drs_showrepl)
         for x in ${SAMBA_DCS} ; do
-            if [ $failure -ne $expected_failure ]; then
-                cat /tmp/samba_drs_showrepl | ${SET_MAILTOOL} -s "FAILURE: unexpected showrepl result between $SETDCFSMO and $x" $EMAIL_REPORT_ADDRESS
+            if [ "${failure}" -ne "${expected_failure}" ]; then
+                ${SET_MAILTOOL} -s "FAILURE: unexpected showrepl result between $SETDCFSMO and $x" $EMAIL_REPORT_ADDRESS < /tmp/samba_drs_showrepl
             fi
-            if [ $success -ne $expected_success ]; then
-                cat /tmp/samba_drs_showrepl | ${SET_MAILTOOL} -s "FAILURE: unexpected showrepl result between $SETDCFSMO and $x" $EMAIL_REPORT_ADDRESS
+            if [ "${success}" -ne "${expected_success}" ]; then
+                ${SET_MAILTOOL} -s "FAILURE: unexpected showrepl result between $SETDCFSMO and $x" $EMAIL_REPORT_ADDRESS < /tmp/samba_drs_showrepl
             fi
         done
     fi
@@ -289,30 +303,32 @@ else
     if [ "${SAMBA_CHECKDB_WITH_LDAPCMD}" = "yes" ]; then
         echo "Checking the DC_With_FSMO (${SAMBA_DC_FSMO}) with SAMBA DC: ${SAMBA_DCS}"
         for x in ${SAMBA_DCS}; do
-            message "Running : ${SET_SAMBATOOL} ldapcmp --filter='whenChanged' ldap://$SAMBA_DC1 ldap://$x "
+            message "Running : ${SET_SAMBATOOL} ldapcmp --filter=\"${SAMBA_LDAPCMD_FILTER}\" ldap://$SAMBA_DC1 ldap://$x "
             message "Please wait.. this can take a while.."
-            ${SET_SAMBATOOL} ldapcmp --filter="whenChanged" ldap://$SAMBA_DC1 ldap://$x  -d0 2>&1 > /tmp/samba_ldapcmp_checkdb
+            #${SET_SAMBATOOL} ldapcmp --filter="${SAMBA_LDAPCMD_FILTER}" ldap://"${SAMBA_DC1}" ldap://"${x}"  -d0 > /tmp/samba_ldapcmp_checkdb
+	    ${SET_SAMBATOOL} ldapcmp --filter="${SAMBA_LDAPCMD_FILTER}" ldap://"$SAMBA_DC1" ldap://"${x}"  -d0 > /tmp/samba_ldapcmp_checkdb 2>&1
             if grep -q FAILURE /tmp/samba_ldapcmp_checkdb; then
-                warning "`cat /tmp/samba_ldapcmp_checkdb`"
+                warning "$(cat /tmp/samba_ldapcmp_checkdb)"
             else
-                good "`cat /tmp/samba_ldapcmp_checkdb`"
+                good "$(cat /tmp/samba_ldapcmp_checkdb)"
             fi
         done
     fi
     echo ".. Next check.. "
     if [ "${SAMBA_CHECKDB_WITH_DRS}" = "yes" ]; then
         message "Running : samba-tool drs showrepl"
-        ${SET_SAMBATOOL} drs showrepl -d0 2>&1 > /tmp/samba_drs_showrepl
-        failure=$(grep -c "failed" /tmp/samba_drs_showrepl)
-        success=$(grep -c "successful" /tmp/samba_drs_showrepl)
+    #    ${SET_SAMBATOOL} drs showrepl -d0 2>&1 > /tmp/samba_drs_showrepl
+	${SET_SAMBATOOL} drs showrepl -d0 > /tmp/samba_drs_showrepl 2>&1
+        failure="$(grep -c "failed" /tmp/samba_drs_showrepl)"
+        success="$(grep -c "successful" /tmp/samba_drs_showrepl)"
         for x in ${SAMBA_DCS} ; do
-            if [ ${failure} -ne ${expected_failure} ]; then
+            if [ "${failure}" -ne "${expected_failure}" ]; then
                 error "failures don't match"
             fi
-            if [ ${success} -ne ${expected_success} ]; then
+            if [ "${success}" -ne "${expected_success}" ]; then
                 error "successes don't match"
             fi
-            if [ ${failure} -eq ${expected_failure} ] && [ ${success} -eq ${expected_success} ]; then
+            if [ "${failure}" -eq "${expected_failure}" ] && [ "${success}" -eq "${expected_success}" ]; then
                 good "No errors found"
             fi
         done
@@ -320,8 +336,10 @@ else
 fi
 
 if [ "${EMAIL_REPORT_ALWAYS}" = "yes" ] && [ -n "${EMAIL_REPORT_ADDRESS}" ]; then
-    cat /tmp/samba_drs_showrepl | ${SET_MAILTOOL} -s "SAMBA CHECK DB : showrepl results" $EMAIL_REPORT_ADDRESS
-    cat /tmp/samba_ldapcmp_checkdb | ${SET_MAILTOOL} -s "SAMBA CHECK DB : ldapcmp results" $EMAIL_REPORT_ADDRESS
+    #cat /tmp/samba_drs_showrepl | ${SET_MAILTOOL} -s "SAMBA CHECK DB : showrepl results" $EMAIL_REPORT_ADDRESS
+    ${SET_MAILTOOL} -s "SAMBA CHECK DB : showrepl results" $EMAIL_REPORT_ADDRESS < /tmp/samba_drs_showrepl
+    #cat /tmp/samba_ldapcmp_checkdb | ${SET_MAILTOOL} -s "SAMBA CHECK DB : ldapcmp results" $EMAIL_REPORT_ADDRESS
+    ${SET_MAILTOOL} -s "SAMBA CHECK DB : ldapcmp results" $EMAIL_REPORT_ADDRESS < /tmp/samba_ldapcmp_checkdb
 fi
 
 if [ "${SETREMOVELOG}" = "yes" ]; then
