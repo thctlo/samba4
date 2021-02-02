aacfc3e02f94255dad5f03fccfd8407ac67d7f9c
Louis van Belle <thctlo@users.noreply.github.com>
2021-02-02 09:30:18 +0100

    Update samba-fix-userhome-recursive.sh

--- samba-fix-userhome-recursive.sh
+++ samba-fix-userhome-recursive.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-V="0.5"
+V="0.5-B1"
+
+# Changelog 0.5 : Added, reset all ACL's and rights and re-apply them.
+#       This is needed to make sure the owner is set correctly.
+#       With old data, the owner can be a non-exising UID.
 
 # This script is use and tested on a Debian Buster Samba MEMBER
 # This is tested with an AD Backend setup.
@@ -12,8 +16,6 @@ V="0.5"
 #
 
 # Copyright (C) Louis van Belle 2020
-# Thanks for helping to improve this to : 
-# dott. Marco Gaiarin
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -28,17 +30,14 @@ V="0.5"
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Into
+# Intro
 # This script is/can be used, as an addon, to the samba-setup-share-folders.sh
 # This is a simple script to re-apply the rights for the user homedir recusively.
 #
 # When you move/migrating or setup clean or whatever, you can use this to fix
 # some rights. After you copied as Administrator, the user is missing its rights
 # on/in there subfolders/files in the homedirs.
-
-# The user needs (MUST HAVE) a UID and "Domain Users" needs a gid, 
-# preffered before you run it.
-# You must set it before you run the script!
+# The user needs a UID and "Domain Users" needs a gid, preffered before you run it.
 
 # It tries to use the USER_SDDL from the samba-setup-share-folders.sh script.
 # If its not detected, then it will use the same defaults after all.
@@ -47,13 +46,6 @@ V="0.5"
 # any newly created folder by the user in the userhome dir will and up with the
 # rights : (POSIX)  username:"domain users"
 # Example, a folder created by the user in his homedir.
-# ! Note, on the part below, you might see numbers and not "domain\040users"
-# or you dont see SYSTEM but a number, this is correct, simple test shows the folloing. 
-## Example 
-# wbinfo -G 2001  = S-1-5-32-545
-# wbinfo -s S-1-5-32-545 = BUILTIN\Users 4
-# wbinfo -n "BUILTIN\Users"  = S-1-5-32-545 SID_ALIAS (4)
-# Your test might, and will most probly, have a different GID number to start with. 
 
 # getfacl TESTING/
 ## file: TESTING/
@@ -76,26 +68,38 @@ V="0.5"
 #default:other::---
 
 
-# Code starts here, it should not be needed to asjust things here.
-# Pickup the current location.
-START_FOLDER="$(pwd)"
-SCRIPT_NAME=$(basename $0)
+#### Variables
+# Enable auto-creating user homedir.
+AUTO_CREATE_USERHOME="yes"
+
+# Add the words that can be detected with getent passwd username
+# and you dont want these userfolders to be created.
+SKIP_USERFILTER="Administrator|dns|remote|sftp|sddl"
 
 # You can define the path to the users shared foldere here.
-SAMBA_SHARE_USERS=""
+SAMBA_SHARE_USERS="/srv/samba/users"
+
+#### Code
+# Code starts here, it should not be needed to asjust things below here.
+
+# Pickup the current location.
+START_FOLDER="$(pwd)"
+SCRIPT_NAME=$(basename "$0")
+
+
 
 # Get the path to where the user folders are from the config files.
 if [ -z "$SAMBA_SHARE_USERS" ]
 then
     if [ -z "${1}" ]
     then
-        SAMBA_SHARE_USERS="$(samba-tool testparm --section-name="users" --parameter-name="path")"
+        SAMBA_SHARE_USERS="$(grep path /etc/samba/*.conf |grep users |grep "path = /" |awk '{ print $NF }' |tail -n1)"
         # did we find the needed settings.
         if [ -z "$SAMBA_SHARE_USERS" ]
         then
             echo "error, unable to detect the users share folder, exiting now."
             echo "This might happing if the users share isn't called users."
-            echo "rerun the script: $SCRIPT_NAME /path/to/samba/users"
+            echo "rerun the script: ${SCRIPT_NAME} /path/to/samba/users"
             exit 1
         fi
     else
@@ -103,40 +107,88 @@ then
     fi
 fi
 
+
 if [ ! -d "${SAMBA_SHARE_USERS}/" ]
 then
     echo "error, unable to detect the users share folder in variable : SAMBA_SHARE_USERS"
-    echo "rerun the script: $SCRIPT_NAME /path/to/samba/users"
+    echo "rerun the script: ${SCRIPT_NAME} /path/to/samba/users"
     exit 1
 else
-    echo "Detected userhomedir basefolder as : ${SAMBA_SHARE_USERS}"
+    echo "Detected userhomedir basefolder as : ${SAMBA_SHARE_USERS}/"
 fi
 
-# cd into dir or exit
-cd ${SAMBA_SHARE_USERS} || exit 1
+# cd into dir the samba/users folder or exit
+cd "${SAMBA_SHARE_USERS}" || exit 1
 
-for user in $(ls)
+# Pre-Create the user homedirs fo all users found with wbinfo -u
+if [ "${AUTO_CREATE_USERHOME}" = "yes" ]
+then
+    for AutoUserHome in $(wbinfo -u)
+    do
+        TEST_INFO_USER="$(getent passwd "${AutoUserHome}"|grep -Ev "${SKIP_USERFILTER}")"
+        if [ -z "${TEST_INFO_USER}" ]
+        then
+            echo "WARNING user ${AutoUserHome} not created. No RFC2307 or removed with SKIP_USERFILTER"
+        elif [ ! -d "${AutoUserHome}/" ]
+        then
+            mkdir "${AutoUserHome}"
+        fi
+    done
+    unset TEST_INFO_USER
+    unset AutoUserHome
+fi
+
+for FindUser in $(ls -d * |grep -Ev "${SCRIPT_NAME}|${SKIP_USERFILTER}")
 do
     # Get the SID of the user.
-    NAME2SID="$(wbinfo --name-to-sid $user |awk '{ print $1 }')"
+    NAME2SID="$(wbinfo --name-to-sid "${FindUser}" |awk '{ print $1 }')"
     if [ ! -z "${NAME2SID}" ]
     then
-        echo "User: ${user}, SID: ${NAME2SID}"
-        if [ -d "${user}" ]
+        echo "#1###############################"
+        echo "Found User: ${FindUser}, SID: ${NAME2SID}"
+        if [ -d "${FindUser}" ]
         then
-            echo "folder : ${user} detected correctly, apply-ing new rights"
-            # Set the correct right on the folder.
-            samba-tool ntacl set "O:S-1-22-1-0G:S-1-22-2-0D:AI(A;OICI;0x001301bf;;;${NAME2SID})(A;ID;0x001200a9;;;S-1-22-2-0)(A;OICIIOID;0x001200a9;;;CG)(A;OICIID;0x001f01ff;;;LA)(A;OICIID;0x001f01ff;;;DA)" "${SAMBA_SHARE_USERS}/${user}"
+            echo "#1###############################"
+            echo "Folder : ${FindUser} detected correctly, apply-ing new rights"
 
-            # but we cant set recursive with samba-tool. (as far i found)            
-            setfacl --recursive --modify user:${user}:rwX,default:user:${user}:rwX "${SAMBA_SHARE_USERS}/${user}"
+            # Remove old ACL's.
+            echo "Removing old ACL's for: ${FindUser}"
+            setfacl --recursive --remove-all  "${SAMBA_SHARE_USERS}/${FindUser}"
+
+            # Make sure we removed Other (everyone) from all files and folders.
+            echo "Recursively removing access for other (everyone) for: ${FindUser}"
+            chmod -R o-rwx "${SAMBA_SHARE_USERS}/${FindUser}/"
+
+            # Set basic POSIX Rights
+            # set all owner rights to root:root (= Administrator:Domain Admins )
+            # without it, migrated files might still have there old UID/GIDs on them.
+            echo "Re-apply root:root on the user homedir (recursivly) for: ${FindUser}"
+            chown -R root:root "${SAMBA_SHARE_USERS}/${FindUser}"
+
+            # We set the user files and subfolders like how that SDDL is setup.
+            echo "Re-apply ${FindUser}:domain users on CONTENT IN the user homedir for: ${FindUser}"
+            chown -R "${FindUser}":"domain users" "${SAMBA_SHARE_USERS}/${FindUser}/"
+
+            # restore owner:group defaults
+            echo "Recursively re-apply-ing rights 770 access for: ${FindUser}"
+            chmod -R 770 "${SAMBA_SHARE_USERS}/${FindUser}/"
+
+            # Set the correct right on the folder.
+            echo "Re-apply SDDL with samba-tool for user: ${FindUser}"
+            samba-tool ntacl set "O:S-1-22-1-0G:S-1-22-2-0D:AI(A;OICI;0x001301bf;;;${NAME2SID})(A;ID;0x001200a9;;;S-1-22-2-0)(A;OICIIOID;0x001200a9;;;CG)(A;OICIID;0x001f01ff;;;LA)(A;OICIID;0x001f01ff;;;DA)" "${SAMBA_SHARE_USERS}/${FindUser}"
+
+            # but we cant set recursive with samba-tool. (as far i found), so we use setfacl.
+            echo "Recursivly re-apply with setfacl enforceing user defaults for user: ${FindUser}"
+            setfacl --recursive --modify user:"${FindUser}":rwX,default:user:"${FindUser}":rwX "${SAMBA_SHARE_USERS}/${FindUser}/"
+
         else
-            echo "Eror, user folder ${SAMBA_SHARE_USERS}/${user} was not detected, skipping!"
+            echo "#2# ERROR ############################"
+            echo "Error, Userfolder ${SAMBA_SHARE_USERS}/${FindUser} was not detected, skipping!"
         fi
     else
-        echo "Usefolder: ${user} exist but unable to find user SID, skipping."
+        echo "#3# ERROR ############################"
+        echo "Userfolder: ${FindUser} exist but unable to find user SID, skipping."
     fi
 done
 
-cd ${START_FOLDER} || exit 1
-
+cd "${START_FOLDER}" || exit 1
